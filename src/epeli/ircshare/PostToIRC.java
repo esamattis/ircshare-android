7d9cc73d6d50eb7e13bf8347ae91dca8e7044880
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-08-08 02:12:47 +0300

    Fix encoding issues.

--- src/epeli/ircshare/PostToIRC.java
+++ src/epeli/ircshare/PostToIRC.java
@@ -3,6 +3,7 @@ package epeli.ircshare;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
@@ -45,11 +46,11 @@ import android.widget.Toast;
 
 public class PostToIRC extends Activity {
 	public static final String PREFS_NAME = "IRCSharePrefs";
-	
+
 	Bundle extras;
 	Intent intent;
 	String action;
-	
+
 	Button send;
 	TextView status;
 	EditText channel;
@@ -57,61 +58,53 @@ public class PostToIRC extends Activity {
 	EditText caption;
 	Spinner network;
 
-
-
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		
-		
+
 		ImageView image = (ImageView) findViewById(R.id.imageview);
-		
+
 		network = (Spinner) findViewById(R.id.network);
 		channel = (EditText) findViewById(R.id.channel);
 		nick = (EditText) findViewById(R.id.nick);
 		caption = (EditText) findViewById(R.id.caption);
-		
+
 		status = (TextView) findViewById(R.id.status);
 		send = (Button) findViewById(R.id.send);
-		
+
 		channel.setText(settings.getString("channel", "#"));
 		nick.setText(settings.getString("nick", ""));
 		network.setSelection(settings.getInt("network", 0));
-		
+
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-		            this, R.array.networks, android.R.layout.simple_spinner_item);
+				this, R.array.networks, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		network.setAdapter(adapter);		
-		
+		network.setAdapter(adapter);
 
 		intent = getIntent();
 		extras = intent.getExtras();
 		action = intent.getAction();
 
-		
-		if (isCalledByGallery()){
+		if (isCalledByGallery()) {
 			setupSmallImg(image);
-		}
-		else {
+		} else {
 			notify("Not called by gallery. Nothing to upload.");
 			setResult(RESULT_OK);
 			finish();
 		}
-		
-		
+
 		send.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				if (!isCalledByGallery()){
-					PostToIRC.this.notify("Not called by gallery. Nothing to upload.");
-				}				
-				
-				
+				if (!isCalledByGallery()) {
+					PostToIRC.this
+							.notify("Not called by gallery. Nothing to upload.");
+				}
+
 				if (channel.getText().length() <= 1) {
 					PostToIRC.this.notify("Set channel");
 					return;
@@ -120,17 +113,15 @@ public class PostToIRC extends Activity {
 					PostToIRC.this.notify("Set nick name");
 					return;
 				}
-				
+
 				new UploadImageTask().execute();
 
-				
 			}
 		});
-		
 
 	}
-	
-	protected void onPause () {
+
+	protected void onPause() {
 		super.onPause();
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 		Editor editor = settings.edit();
@@ -139,119 +130,141 @@ public class PostToIRC extends Activity {
 		editor.putInt("network", network.getSelectedItemPosition());
 		editor.commit();
 	}
-	
-	private boolean isCalledByGallery(){
-		return Intent.ACTION_SEND.equals(action) && extras.containsKey(Intent.EXTRA_STREAM);
+
+	private boolean isCalledByGallery() {
+		return Intent.ACTION_SEND.equals(action)
+				&& extras.containsKey(Intent.EXTRA_STREAM);
 	}
-	
+
 	private void notify(CharSequence text) {
 		Context context = getApplicationContext();
 		Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
-		toast.show();		
+		toast.show();
 		status.setText(text);
 	}
-	
+
 	private InputStream getImgInputStream() throws FileNotFoundException {
 		// Get resource path from intent callee
 		Uri uri = (Uri) extras.getParcelable(Intent.EXTRA_STREAM);
-		
+
 		// Query gallery for camera picture via
 		// Android ContentResolver interface
 		ContentResolver cr = getContentResolver();
 		return cr.openInputStream(uri);
-		
+
 	}
-	
+
 	private Uri getImgUri() {
 		return (Uri) extras.getParcelable(Intent.EXTRA_STREAM);
 	}
-	
+
 	private AssetFileDescriptor getImgFile() throws FileNotFoundException {
 		ContentResolver cr = getContentResolver();
 		return cr.openAssetFileDescriptor(getImgUri(), "r");
 	}
-	
+
 	private String getImgMimeType() {
 		ContentResolver cr = getContentResolver();
 		return cr.getType(getImgUri());
 	}
-	
+
 	private void setupSmallImg(ImageView img) {
-		
+
 		Bitmap bitmap;
 		try {
 			bitmap = BitmapFactory.decodeStream(getImgInputStream());
-			double scale = (double) bitmap.getHeight() / (double) bitmap.getWidth();
-			img.setImageBitmap( Bitmap.createScaledBitmap(bitmap, 400, (int) (400.0 * scale ), true) );
+			double scale = (double) bitmap.getHeight()
+					/ (double) bitmap.getWidth();
+			img.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 400,
+					(int) (400.0 * scale), true));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
-	
+
 	private class UploadImageTask extends AsyncTask<Void, Void, Void> {
-		
+
 		String res;
 		String err;
-		
-		private void uploadImage(){
-			
+
+		/**
+		 * Escape non-ascii characters and try to compatible with
+		 * querystring.unescape of Node.js.
+		 * 
+		 * @param s
+		 * @throws UnsupportedEncodingException
+		 */
+		private StringBody escapeToStringBody(String s)
+				throws UnsupportedEncodingException {
+			s = URLEncoder.encode(s);
+			s = s.replace("+", "%20");
+			return new StringBody(s);
+
+		}
+
+		private void uploadImage() {
+
 			HttpClient httpclient = new DefaultHttpClient();
-			httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
-			HttpPost httppost = new HttpPost("http://kamino.kortex.jyu.fi:1337/upload");
+			httpclient.getParams().setParameter(
+					CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+			HttpPost httppost = new HttpPost(
+					"http://kamino.kortex.jyu.fi:1337/upload");
 			try {
+
+				MultipartEntity entity = new MultipartEntity(
+						HttpMultipartMode.STRICT, null,
+						Charset.forName("UTF-8"));
+
+				entity.addPart("nick", escapeToStringBody(nick.getText()
+						.toString()));
 				
-				
-			  MultipartEntity entity = new MultipartEntity(HttpMultipartMode.STRICT, null, Charset.forName("UTF-8") );
-			  
-			  entity.addPart("nick", new StringBody(nick.getText().toString()));
-			  entity.addPart("channel", new StringBody(channel.getText().toString()));
-			  entity.addPart("caption", new StringBody(URLEncoder.encode(caption.getText().toString())));
-			  entity.addPart("network", new StringBody(network.getSelectedItem().toString()));
-			  
-			  
-			  entity.addPart("picdata", new InputStreamBody(getImgInputStream(), getImgMimeType(), "pic.jpg"));
-			  
-			  httppost.setEntity(entity);
-			  ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			  res = httpclient.execute(httppost, responseHandler);
-			  
+				entity.addPart("channel", escapeToStringBody(channel.getText()
+						.toString()));
+
+				entity.addPart("caption", escapeToStringBody(caption.getText()
+						.toString()));
+
+				entity.addPart("network", escapeToStringBody(network
+						.getSelectedItem().toString()));
+
+				entity.addPart("picdata", new InputStreamBody(
+						getImgInputStream(), getImgMimeType(), "pic.jpg"));
+
+				httppost.setEntity(entity);
+				ResponseHandler<String> responseHandler = new BasicResponseHandler();
+				res = httpclient.execute(httppost, responseHandler);
+
 			} catch (ClientProtocolException e) {
 				err = e.toString();
 			} catch (IOException e) {
 				err = e.toString();
 			}
-			     
-			
+
 		}
-	
 
 		@Override
 		protected Void doInBackground(Void... params) {
 			uploadImage();
 			return null;
 		}
-		
 
 		public void onPreExecute() {
 			PostToIRC.this.notify("Sending image...");
 		}
-		
+
 		public void onPostExecute(Void n) {
 			if (err == null) {
-				ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+				ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
 				clipboard.setText(res);
-				PostToIRC.this.notify("Image sent! Copied url to clipboard: " + res);
-			}
-			else {
+				PostToIRC.this.notify("Image sent! Copied url to clipboard: "
+						+ res);
+			} else {
 				PostToIRC.this.notify("Error sending picture: " + err);
 			}
 		}
-	
 
-	
 	}
-	
+
 }
